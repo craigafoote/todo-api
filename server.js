0408249122457fcb7ce89db05207398417132f23
Craig Foote <foote.craig@gmail.com>
2017-10-24 14:33:08 -0500

    added delete todos route

--- server.js
+++ server.js
@@ -44,6 +44,18 @@ app.post('/todos', function(req, res){
 	res.json(body);
 });
 
+app.delete('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedToDo = _.findWhere(todos, {id: todoId});
+
+	if(matchedToDo){
+		todos = _.without(todos, matchedToDo);
+		res.json(matchedToDo);
+	} else {
+		res.status(404).json({"error" : "no todo found with that id"});		
+	}
+});
+
 app.listen(PORT, function(){
 	console.log("Express listening on port" + PORT);
 });
