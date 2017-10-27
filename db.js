ba9c2b19da4ac9a63b6065536a3f7b4c2ee370b5
Craig Foote <foote.craig@gmail.com>
2017-10-27 13:20:03 -0500

    correct error in db.js

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
+
 if(env === 'production'){
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
-		dialect: 'postgres'
+		'dialect': 'postgres'
 	});
 } else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
